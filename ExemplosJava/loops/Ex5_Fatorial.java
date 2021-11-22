5e08e68d3cb52a923db1818489cdc34d8d6ee369
Weslley-Prado <84636703+Weslley-Prado@users.noreply.github.com>
2021-11-22 15:31:23 -0300

    Create Ex5_Fatorial.java

---
+++ ExemplosJava/loops/Ex5_Fatorial.java
@@ -0,0 +1,23 @@
+package br.com.dio.exercicios.loops;
+import java.util.Scanner;
+
+public class Ex5_Fatorial {
+
+	public static void main(String[] args) {
+		
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);			
+		int multiplicacao = 1;
+	
+		
+		System.out.println("Fatorial:");
+		int fatorial = scan.nextInt();	
+		
+		System.out.print(fatorial+ "! =");		
+		for (int i = fatorial; i>=1 ; i--) {
+				multiplicacao = multiplicacao * i;
+			}
+		
+          System.out.println(multiplicacao);
+	}
+}
